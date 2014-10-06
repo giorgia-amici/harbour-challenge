0944ab67171a76207828ed5a3d15b0b97118f6f8
Giorgia <giorgia1london@gmail.com>
2014-10-06 21:44:52 +0100

    ship.rb

--- lib/ship.rb
+++ lib/ship.rb
@@ -7,15 +7,3 @@ class Ship
 
 end
 
-
-
-
-
-
-
-
-# i will give you a hint, that may help you or not, don't think in a
-# delivery method think how delivery works, imagine you are a lorry or a
-# ship, and there is a harbour, and imagine what needs to happen in
-# order for a lorry-ship to pickup a container, how they should interact
-# with the harbour

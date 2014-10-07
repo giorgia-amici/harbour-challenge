14dc7b192374cc1c7df79cdd3fcf12166e23fd4b
Giorgia <giorgia1london@gmail.com>
2014-10-07 20:36:52 +0100

    container.rb

--- lib/container.rb
+++ lib/container.rb
@@ -1,21 +1,9 @@
 class Container
-	attr_accessor :id, :origin, :destination
+	attr_accessor :id, :origin, :destination, :delivery_date
 
 	def initialize
 		@id = rand(1889..43556)
-		@origin = ['Rome', 'Vigo', 'Liverpool', 'New York', 'Beijing', 'Naples','Marseille'].shuffle[0]
-		@destination = 'Rome'
-		#, 'Vigo', 'Liverpool', 'New York', 'Beijing', 'Naples','Marseille'].shuffle[0]
-		# while @origin == @destination
-		# 	@destination = ['Rome', 'Vigo', 'Liverpool', 'New York', 'Beijing', 'Naples','Marseille'].shuffle[0]
-		# end
-
 	end 
-
-	# def delivery_date
-	# 	 @delivery_date = (Date.today + rand(1..7)).to_s
-	# end
-
 
 end	
 

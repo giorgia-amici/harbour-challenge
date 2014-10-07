dce90c93d19507c4e26be324ebdabf6b0d6ea490
Giorgia <giorgia1london@gmail.com>
2014-10-07 20:04:39 +0100

    harbour.rb

--- lib/harbour.rb
+++ lib/harbour.rb
@@ -5,6 +5,7 @@ class Harbour
 		@cargo = []
 		@fleet = []
 		@infantry = [] 
+		@containers_to_be_delivered = []
 	end
 
 	def ready_to_be_picked(container)
@@ -28,15 +29,8 @@ class Harbour
 		@infantry.include?(lorry)
 	end
 
-# if infantry?(mean) == true or fleet?(mean) == true
 
-
-	def which_mean(container)
-		container.mean_of_transport
-	end
-
-
-	def find_mean_into_harbour(mean)
+	def find_into_harbour(mean)
 		if mean.class == Ship
 			@fleet.index(mean)
 		elsif mean.class == Lorry
@@ -44,20 +38,34 @@ class Harbour
 		end	
 	end
 
-	def pick_cargo(container, mean)
-			what_picks = which_mean(container)
-			if what_picks == 'lorry' and mean.class == Lorry
-				position = find_mean_into_harbour(mean)
-				container.picked_from_harbour = Date.today.to_s
-				@infantry[position].on_truck << container
-			puts "I am gonna be picked by a lorry"
-			elsif what_picks == 'ship' and mean.class == Ship
-				position = find_mean_into_harbour(mean)
-				container.picked_from_harbour = Date.today.to_s
-				@fleet[position].on_board << container
-			puts "I am gonna be picked by a ship"
-		else
-			puts 'I cannot be carried by this means of transport '
-		end
+	# def same_destination?(container,mean)
+	# 	if container.destination == mean.destination 
+	# 		true
+	# 	end
+	# end
+
+	def pick_up(container)
+		@fleet.select{|ship| ship.destination == container.destination} 
+		@infantry.select{|lorry| lorry.destination == container.destination}
+
+		#IF BOTH ARE NOT FULL
 	end
+
+
+
+
+# def pick_up(container, mean)
+# 	@cargo.each do |container|
+# 	if container.destination == mean.destination
+# 		mean.on_board << container
+# 		@containers_to_be_deliverd << container
+# 	end
+# end
+
+
+
+	
+
+
+
 end  		 	
